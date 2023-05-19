41ca8026bb694b1a32275c084702f614f9582f48
andiharun29 <andiharunalrasyid14@gmail.com>
2023-05-19 17:38:55 +0700

    masukkan traverse ke dalm list

--- siNglylinkedlist0/siNglylinkedlist0.cpp
+++ siNglylinkedlist0/siNglylinkedlist0.cpp
@@ -108,3 +108,19 @@ bool List::search(int nim, Node * *previous, Node * *current)
 	return (*current != NULL);
 }
 
+void List::traverse() /* mengunjungi dari membaca data dalam list */
+{
+	if (listempty())
+		cout << "\nList Kosong\n";
+	else
+	{
+		cout << endl << "Data didalam list adalah:" << endl;
+		Node* currentNode;
+		for (currentNode = START; currentNode != NULL; currentNode = currentNode->next)
+		{
+			cout << currentNode->noMhs << "  " << currentNode->nama << "\n";
+		}
+		cout << endl;
+	}
+}
+
